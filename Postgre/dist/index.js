51fbca1e2467cae8d47a494e5181d82645f1cc44
kunnal-tyagi <kunaltyyagi@gmail.com>
2025-10-31 19:25:51 +0000

    postgre

---
+++ Postgre/dist/index.js
@@ -0,0 +1,51 @@
+import { Client } from 'pg';
+const client = new Client({
+    connectionString: "postgresql://neondb_owner:npg_9fB7FTYhgwRv@ep-orange-smoke-a1r4stwg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+});
+async function CreateTable() {
+    try {
+        await client.connect();
+        console.log("✅ Connected to PostgreSQL");
+        await client.query(`DROP TABLE IF EXISTS users;`);
+        const result = await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(50) UNIQUE,
+        password VARCHAR(225) NOT NULL,
+        age INT,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+        console.log("✅ Users table created successfully!");
+    }
+    catch (err) {
+        console.error("❌ Error creating table:", err);
+    }
+}
+// ✅ Insert function
+async function InsertData(username, password, age) {
+    try {
+        const inserting = `
+      INSERT INTO users (username, password, age)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+        const values = [username, password, age];
+        const res = await client.query(inserting, values);
+        console.log("✅ Inserted data:", res.rows[0]);
+    }
+    catch (err) {
+        console.error("❌ Error during insertion:", err);
+    }
+    finally {
+        await client.end();
+        console.log("🔒 Connection closed");
+    }
+}
+// ✅ Run in proper sequence
+(async () => {
+    await CreateTable(); // wait for table creation
+    await InsertData("Kunnal", "2809932", 20);
+})();
+//# sourceMappingURL=index.js.map
