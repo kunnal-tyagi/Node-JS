a9b17812878de2543167a3b8a7b9e4f5ad8d1e6d
kunnal-tyagi <kunaltyyagi@gmail.com>
2025-12-11 15:56:05 +0530

    liveChat

--- DEVTINDER/src/routes/chat.js
+++ DEVTINDER/src/routes/chat.js
@@ -1,8 +1,19 @@
 const express=require("express");
 const chatRouter=express.Router();
 const Chat = require("../models/chat.js");
+const UserAuth = require("../Middlewares/verify.js");
+chatRouter.get('/chat',UserAuth,async (req,res)=>{
+  const {targetUserId}=req.body;
+  const userID=req.user._id;
+  try{
+    let chat=await Chat.findOne({participants:{$all:[userID,targetUserId]}});
+    if(!chat){
 
-chatRouter.get('/chat')
+    }
+  }catch(err){
+    res.status(500).json({message:"Server Error"});
+  }
+})
 
 
 module.exports=chatRouter;
